c3d13a03dd14b5571b87461eee7aebd87dd1a22f
ramdaskadam20 <ramdaskdm2012@gmail.com>
2025-06-26 18:24:59 +0530

    demo

---
+++ sample_pro/src/sample/Demo.java
@@ -0,0 +1,27 @@
+package sample;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		
+	
+		int n1=0,n2=1,n3;
+		
+	for(int i=0;i<=10;i++)
+	{
+		
+		
+		
+		n3=n1+n2;
+		
+		System.out.println(n3);
+		
+		n1=n2;
+		n2=n3;
+	}
+	
+
+	}
+
+}
